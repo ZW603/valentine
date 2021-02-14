9d4b21642afc430e0d156cae4a5aeba39449856e
ZW603 <75135647+ZW603@users.noreply.github.com>
2021-02-14 02:02:32 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Breeze05" && b=="Vivian05")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
